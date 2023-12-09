227ed00fcdbd4ddf11d364f8ab8e9a2bece3c9df
fnt <faint0rz@gmail.com>
2023-12-09 12:57:00 +0100

    Day 3 solution

--- day3/main.go
+++ day3/main.go
@@ -3,44 +3,153 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
+	"unicode"
 )
 
-type part struct {
-	value      string
-	startIndex int
-	endIndex   int
-	lenght     int
+type neighbour struct {
+	x    int
+	y    int
+	char rune
+}
+
+type symbol struct {
+	char       rune
+	x          int
+	y          int
+	neighbours []neighbour
 }
 
 func main() {
+
 	file, _ := os.ReadFile("./input.txt")
 	lines := strings.Split(string(file), "\n")
 
-	partsMap := map[int][]part{}
-	for index, line := range lines {
-		partsInLine := partsPerLine(line)
-		partsMap[index] = partsInLine
+	parts := map[string]int{}
+	gearValues := []int{}
+	symbols := indexLines(lines)
+	for _, symbol := range symbols {
+		symbol.getNeighbours(lines)
+
+		for _, neightbour := range symbol.neighbours {
+			if unicode.IsDigit(neightbour.char) {
+				key, value := getPartNumber(neightbour, lines)
+
+				if _, exists := parts[key]; !exists {
+					parts[key] = value
+				}
+
+			}
+		}
+
+		gearValue := 0
+		if symbol.char == '*' {
+			gearNeighboursMap := map[string]int{}
+			gearParts := []int{}
+			for _, neighbour := range symbol.neighbours {
+				if unicode.IsDigit(neighbour.char) {
+					key, value := getPartNumber(neighbour, lines)
+					if _, exists := gearNeighboursMap[key]; !exists {
+						gearNeighboursMap[key] = value
+						gearParts = append(gearParts, value)
+					}
+				}
+			}
+
+			if len(gearParts) == 2 {
+				gearValue = gearParts[0] * gearParts[1]
+			}
+
+			gearValues = append(gearValues, gearValue)
+		}
 	}
 
-	fmt.Printf("partsMap: %v\n", partsMap)
+	totalParts := 0
+	for _, part := range parts {
+		totalParts += part
+	}
+
+	totalGears := 0
+	for _, gearValue := range gearValues {
+		totalGears += gearValue
+	}
+
+	fmt.Println(totalParts)
+	fmt.Println(totalGears)
 }
 
-func partsPerLine(line string) (parts []part) {
+func getPartNumber(neightbour neighbour, lines []string) (key string, part int) {
 
-	startIndex := 0
-	for index, symbol := range line {
-		if symbol != '.' && startIndex == 0 {
-			startIndex = index
-		}
+	key = fmt.Sprintf("%d,%d", neightbour.x, neightbour.y)
+	r := regexp.MustCompile(`[0-9]{1,3}`)
+	pos := neightbour.y
 
-		if symbol == '.' && startIndex != 0 {
-			diff := index - startIndex
-			value := line[startIndex:index]
-			parts = append(parts, part{startIndex: startIndex, endIndex: index, lenght: diff, value: value})
-			startIndex = 0
+	for i := neightbour.y - 1; i >= 0; i-- {
+		if unicode.IsDigit(rune(lines[neightbour.x][i])) {
+			pos = i
+		} else {
+			break
 		}
 	}
 
-	return parts
+	lineSubString := lines[neightbour.x][pos:]
+
+	partAsString := r.FindString(lineSubString)
+	if partAsString != "" {
+		nr, err := strconv.Atoi(partAsString)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		key = fmt.Sprintf("%d,%d", neightbour.x, pos)
+		return key, nr
+	}
+	return key, 0
+}
+
+func (sym *symbol) getNeighbours(lines []string) {
+	addNeightbour := func(x, y int) {
+		neighbourToAdd := checkNeightbour(x, y, lines)
+		sym.neighbours = append(sym.neighbours, neighbour{x: x, y: y, char: neighbourToAdd})
+	}
+
+	addNeightbour(sym.x-1, sym.y)
+	addNeightbour(sym.x, sym.y-1)
+	addNeightbour(sym.x+1, sym.y)
+	addNeightbour(sym.x, sym.y+1)
+	addNeightbour(sym.x-1, sym.y-1)
+	addNeightbour(sym.x+1, sym.y+1)
+	addNeightbour(sym.x-1, sym.y+1)
+	addNeightbour(sym.x+1, sym.y-1)
+
+}
+
+func checkNeightbour(x, y int, lines []string) (neighbour rune) {
+	neighbour = '.'
+	withinGrid := (x >= 0 && x < len(lines[0]) && y >= 0 && y < len(lines))
+	if withinGrid {
+		neighbour = rune(lines[x][y])
+	}
+
+	return neighbour
+
+}
+
+func indexLines(lines []string) (symbols []symbol) {
+
+	for x, line := range lines {
+		for y, char := range line {
+			if char == '.' {
+				continue
+			}
+
+			if unicode.IsSymbol(char) || unicode.IsPunct(char) {
+				symbols = append(symbols, symbol{x: x, y: y, char: char})
+			}
+		}
+	}
+	return symbols
 }
