cd6bbfcc01577dc0e2b7739355d482958a0709a2
fnt <faint0rz@gmail.com>
2023-12-12 23:51:53 +0100

    brute force

--- day5/day5.go
+++ day5/day5.go
@@ -26,34 +26,121 @@ type mapInfo struct {
 
 type mapRange struct {
 	des_start    int
+	des_end      int
 	source_start int
 	source_end   int
 }
 
 type seed struct {
-	id          int
-	soil        int
-	fertilizer  int
-	water       int
-	light       int
-	temperature int
-	humidity    int
-	location    int
+	id       int
+	location int
 }
 
 func Run() {
-	file := utils.GetData("5", true)
+	file := utils.GetData("5", false)
 	file = strings.Trim(file, "\n")
 	lines := strings.Split(file, "\n\n")
 
 	defer utils.Timer("day5")()
 
 	seeds := utils.ConvertStringSliceToIntSlice(strings.Split(lines[0], " ")[1:])
-	Part1(seeds, lines)
+	part1(seeds, lines)
+
+	seedRanges := seedRange(seeds)
+	part2(seedRanges, lines)
 
 }
 
-func Part1(seeds []int, lines []string) {
+func part2(seeds []mapRange, lines []string) {
+
+	mapTypes := getMapTypes(lines)
+
+	seedToSoilMap := sourceToDestination(mapTypes[SOIL])
+	soilToFertMap := sourceToDestination(mapTypes[FERTILIZER])
+	fertToWaterMap := sourceToDestination(mapTypes[WATER])
+	waterToLight := sourceToDestination(mapTypes[LIGHT])
+	lightToTemp := sourceToDestination(mapTypes[TEMPERATURE])
+	tempToHumid := sourceToDestination(mapTypes[HUMIDITY])
+	humidToLocation := sourceToDestination(mapTypes[LOCATION])
+
+	diff := 0
+	for _, seedRange := range seeds {
+		d := seedRange.source_end - seedRange.source_start
+		diff += d
+	}
+
+	lowestLocation := 0
+	for _, seedRange := range seeds {
+		for i := seedRange.source_start; i < seedRange.source_end; i++ {
+
+			soil := addSpecsToSeed(i, &seedToSoilMap)
+			fertilizer := addSpecsToSeed(soil, &soilToFertMap)
+			water := addSpecsToSeed(fertilizer, &fertToWaterMap)
+			light := addSpecsToSeed(water, &waterToLight)
+			temperature := addSpecsToSeed(light, &lightToTemp)
+			humidity := addSpecsToSeed(temperature, &tempToHumid)
+			location := addSpecsToSeed(humidity, &humidToLocation)
+
+			if lowestLocation == 0 || location < lowestLocation {
+				lowestLocation = location
+			}
+		}
+	}
+
+	fmt.Println(lowestLocation)
+}
+
+func seedRange(seeds []int) []mapRange {
+
+	seedRanges := []mapRange{}
+	for i := 0; i < len(seeds); i += 2 {
+		startSeed := seeds[i]
+		endSeed := startSeed + seeds[i+1]
+		seedRanges = append(seedRanges, mapRange{source_start: seeds[i], source_end: endSeed})
+
+	}
+
+	return seedRanges
+}
+
+func part1(seeds []int, lines []string) {
+
+	mapTypes := getMapTypes(lines)
+
+	seedToSoilMap := sourceToDestination(mapTypes[SOIL])
+	soilToFertMap := sourceToDestination(mapTypes[FERTILIZER])
+	fertToWaterMap := sourceToDestination(mapTypes[WATER])
+	waterToLight := sourceToDestination(mapTypes[LIGHT])
+	lightToTemp := sourceToDestination(mapTypes[TEMPERATURE])
+	tempToHumid := sourceToDestination(mapTypes[HUMIDITY])
+	humidToLocation := sourceToDestination(mapTypes[LOCATION])
+
+	specifiedSeeds := make([]seed, 0, len(seeds))
+	for _, s := range seeds {
+		newSeed := seed{id: s}
+
+		soil := addSpecsToSeed(newSeed.id, &seedToSoilMap)
+		fertilizer := addSpecsToSeed(soil, &soilToFertMap)
+		water := addSpecsToSeed(fertilizer, &fertToWaterMap)
+		light := addSpecsToSeed(water, &waterToLight)
+		temperature := addSpecsToSeed(light, &lightToTemp)
+		humidity := addSpecsToSeed(temperature, &tempToHumid)
+		newSeed.location = addSpecsToSeed(humidity, &humidToLocation)
+
+		specifiedSeeds = append(specifiedSeeds, newSeed)
+	}
+
+	lowestLocation := 0
+	for _, s := range specifiedSeeds {
+		if lowestLocation == 0 || s.location < lowestLocation {
+			lowestLocation = s.location
+		}
+	}
+
+	fmt.Println(lowestLocation)
+}
+
+func getMapTypes(lines []string) map[string][]mapInfo {
 	maps := lines[1:]
 
 	mapTypes := map[string][]mapInfo{}
@@ -75,43 +162,12 @@ func Part1(seeds []int, lines []string) {
 
 		mapTypes[mapTitle] = mapInfos
 	}
-
-	seedToSoilMap := sourceToDestination(mapTypes[SOIL])
-	soilToFertMap := sourceToDestination(mapTypes[FERTILIZER])
-	fertToWaterMap := sourceToDestination(mapTypes[WATER])
-	waterToLight := sourceToDestination(mapTypes[LIGHT])
-	lightToTemp := sourceToDestination(mapTypes[TEMPERATURE])
-	tempToHumid := sourceToDestination(mapTypes[HUMIDITY])
-	humidToLocation := sourceToDestination(mapTypes[LOCATION])
-
-	specifiedSeeds := []seed{}
-	for _, s := range seeds {
-		newSeed := seed{id: s}
-
-		newSeed.soil = AddSpecsToSeed(newSeed.id, seedToSoilMap)
-		newSeed.fertilizer = AddSpecsToSeed(newSeed.soil, soilToFertMap)
-		newSeed.water = AddSpecsToSeed(newSeed.fertilizer, fertToWaterMap)
-		newSeed.light = AddSpecsToSeed(newSeed.water, waterToLight)
-		newSeed.temperature = AddSpecsToSeed(newSeed.light, lightToTemp)
-		newSeed.humidity = AddSpecsToSeed(newSeed.temperature, tempToHumid)
-		newSeed.location = AddSpecsToSeed(newSeed.humidity, humidToLocation)
-
-		specifiedSeeds = append(specifiedSeeds, newSeed)
-	}
-
-	lowestLocation := 0
-	for _, s := range specifiedSeeds {
-		if lowestLocation == 0 || s.location < lowestLocation {
-			lowestLocation = s.location
-		}
-	}
-
-	fmt.Println(lowestLocation)
+	return mapTypes
 }
 
-func AddSpecsToSeed(sourceId int, ranges []mapRange) int {
+func addSpecsToSeed(sourceId int, ranges *[]mapRange) int {
 
-	for _, mapRange := range ranges {
+	for _, mapRange := range *ranges {
 		if sourceId > mapRange.source_end || sourceId < mapRange.source_start {
 			continue
 		}
@@ -135,6 +191,7 @@ func sourceToDestination(ranges []mapInfo) []mapRange {
 		end := r.source_range + r.lenght_range - 1
 		mr := mapRange{
 			des_start:    r.destination_range,
+			des_end:      r.destination_range + r.lenght_range - 1,
 			source_start: r.source_range,
 			source_end:   end}
 
